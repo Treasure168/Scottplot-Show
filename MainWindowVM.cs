802129834db9df74071113f398a313dd7a578f57
Legion <Legion@DESKTOP-6N31BOD>
2024-06-07 22:52:04 +0800

    添加项目文件。

---
+++ MainWindowVM.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfApp1.Base;
+
+namespace WpfApp1
+{
+    public class MainWindowVM : BaseViewModel
+    {
+        private ObservableCollection<double[]> _plotData;
+
+        public ObservableCollection<double[]> PlotData
+        {
+            get { return _plotData; }
+            set
+            {
+                if (_plotData != value)
+                {
+                    _plotData = value;
+                    OnpropertyChanged(nameof(PlotData));
+                }
+            }
+        }
+        public ICommand UpdateDataCommand { get; }
+
+        public MainWindowVM()
+        {
+            PlotData = new ObservableCollection<double[]>();
+            //{
+            //    new double[] {1, 2, 3, 4, 5},
+            //    new double[] {2, 4, 6, 8, 10},
+            //    new double[] {5, 10, 15, 20, 25},
+            //    new double[] {3, 6, 9, 12, 15}
+            //};
+            UpdateDataCommand = new RelayCommand(_ => UpdateData());
+        }
+
+      
+
+        private void UpdateData()
+        {
+            Random rand = new Random();
+            var newPlotData = new ObservableCollection<double[]>();
+            //PlotData.Clear();
+
+            for (int i = 0; i < 4; i++)
+            {
+                double[] newData = new double[5];
+                for (int j = 0; j < 5; j++)
+                {
+                    newData[j] = rand.NextDouble() * 10;
+                }
+                newPlotData.Add(newData);
+            }
+              PlotData = newPlotData;
+        }
+
+    }
+}
---
+++ PlotControl.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// PlotControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class PlotControl : UserControl
+    {
+        public ObservableCollection<double[]> DataSeries
+        {
+            get { return (ObservableCollection<double[]>)GetValue(DataSeriesProperty);}
+            set { 
+                SetValue(DataSeriesProperty, value);
+                if (value != null)
+                {
+                    value.CollectionChanged += DataSeries_CollectionChanged;
+                }
+            }
+        }
+        public static readonly DependencyProperty DataSeriesProperty =
+        DependencyProperty.Register("DataSeries", typeof(ObservableCollection<double[]>), typeof(PlotControl), new PropertyMetadata(new ObservableCollection<double[]>(), OnDataSeriesChanged));
+        //public static readonly DependencyProperty DataSeriesProperty =
+        // DependencyProperty.Register("DataSeries", typeof(ObservableCollection<double[]>), typeof(PlotControl), new PropertyMetadata(new ObservableCollection<double[]>()));
+        private static void OnDataSeriesChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is PlotControl control && e.NewValue is ObservableCollection<double[]> newSeries)
+            {
+                control.PlotData(newSeries);
+            }
+        }
+        public PlotControl()
+        {
+            InitializeComponent();
+        }
+        private void DataSeries_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            PlotData(DataSeries);
+        }
+        private void PlotData(ObservableCollection<double[]> dataSeries)
+        {
+            var plt = wpfPlot.Plot;
+            plt.Clear();
+
+            if (dataSeries != null)
+            {
+                for (int i = 0; i < dataSeries.Count; i++)
+                {
+                    double[] data = dataSeries[i];
+                    plt.AddSignal(data, label: $"Series {i + 1}");
+                }
+            }
+
+            plt.Legend();
+            wpfPlot.Refresh();
+        }
+    }
+}
